fc19095a64bf76aa2b3884a19eab15e8170ff5d5
Geoffroy Megert <geoffroy.megert@he-arc.ch>
2015-12-12 23:50:51 +0100

    Codage des Backings Beans pour l'affichage des listes des personnes et des films.

---
+++ src/java/ch/hearc/ig/odi/moviemanager/presentation/bean/MoviesBean.java
@@ -0,0 +1,44 @@
+package ch.hearc.ig.odi.moviemanager.presentation.bean;
+
+import ch.hearc.ig.odi.moviemanager.business.Movie;
+import ch.hearc.ig.odi.moviemanager.services.Services;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.ArrayDataModel;
+import javax.faces.model.DataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Cette classe est lié à la page "index.xhtml".
+ * Elle permet la gestion des listes de films.
+ * 
+ * @author Geoffroy Megert <geoffroy.megert@he-arc.ch>
+ */
+// Le nom employé sur les pages xhtml pour faire appel à ce Backing Bean.
+@Named(value = "moviesBean")
+// Cycle de vie du Backing Bean.
+// Ce Backing Bean ne manipule pas d'objet necessitant d'être mémorisé plus d'une requête.
+@RequestScoped
+public class MoviesBean {
+    
+    /** La liste de films. */
+    // Les DataModel sont des composants adaptés aux JavaServer Faces.
+    private DataModel<Movie> movies;
+    
+    /** Services mise à disposition des programmeurs. */
+    // L'injection permet la gestion automatique de l'instanciation des services.
+    @Inject
+    private Services service;
+    
+    /**
+     * Permet d'obtenir la liste des films.
+     * 
+     * @return La liste des films.
+     */
+    public DataModel<Movie> getMovies() {
+        movies = new ArrayDataModel<>();
+        movies.setWrappedData(service.getMoviesList());
+        
+        return movies;
+    }
+}
---
+++ src/java/ch/hearc/ig/odi/moviemanager/presentation/bean/PeopleBean.java
@@ -0,0 +1,44 @@
+package ch.hearc.ig.odi.moviemanager.presentation.bean;
+
+import ch.hearc.ig.odi.moviemanager.business.Person;
+import ch.hearc.ig.odi.moviemanager.services.Services;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.ArrayDataModel;
+import javax.faces.model.DataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Cette classe est lié à la page "index.xhtml".
+ * Elle permet la gestion des listes de personnes.
+ * 
+ * @author Geoffroy Megert <geoffroy.megert@he-arc.ch>
+ */
+// Le nom employé sur les pages xhtml pour faire appel à ce Backing Bean.
+@Named(value = "peopleBean")
+// Cycle de vie du Backing Bean.
+// Ce Backing Bean ne manipule pas d'objet necessitant d'être mémorisé plus d'une requête.
+@RequestScoped
+public class PeopleBean {
+    
+    /** La liste de personnes. */
+    // Les DataModel sont des composants adaptés aux JavaServer Faces.
+    private DataModel<Person> people;
+    
+    /** Services mise à disposition des programmeurs. */
+    // L'injection permet la gestion automatique de l'instanciation des services.
+    @Inject
+    private Services services;
+    
+    /**
+     * Permet d'obtenir la liste des personnes.
+     * 
+     * @return La liste des personnes.
+     */
+    public DataModel<Person> getPeople() {
+        people = new ArrayDataModel<>();
+        people.setWrappedData(services.getPeopleList());
+        
+        return people;
+    }
+}
